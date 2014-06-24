fb9091dcef3135c8ff2d3c374528e0b01ba11aa5
Ovidiu Miron <omiron1@yahoo.com>
2014-06-24 11:45:10 +0300

    clean zope version

--- src/omiron.blog/omiron/blog/interfaces.py
+++
@@ -1,9 +0,0 @@
-from zope.interface import Interface
-from zope.schema import TextLine
-from zope.schema import Text
-
-
-class IBlogFolder(Interface):
-    title = TextLine(title=u"Title", required=True, default=u"meme")
-    author = TextLine(title=u"Author", required=True)
-    text = Text(title=u"Text", required=False)
